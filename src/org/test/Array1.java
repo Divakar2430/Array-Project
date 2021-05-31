6dfa6769bc85cb9ed672e170ac09613676fa8b3b
Divakar2430 <divakarmuthu1994@gmail.com>
2021-05-31 12:54:47 +0530

    Added Array Programs

---
+++ src/org/test/Array1.java
@@ -0,0 +1,17 @@
+package org.test;
+
+public class Array1 {
+public static void main(String[] args) {
+	int []a=new int[5];
+	a[0]=10;
+	a[1]=20;
+	a[2]=30;
+	a[3]=40;
+	a[4]=50;
+	a[2]=300;
+	for(int k:a) {
+	System.out.println(k);
+}
+}
+}
+
